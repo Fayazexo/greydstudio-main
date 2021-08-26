0a9fb3d7ec1baba3a8fc69b8b1e5bbb26b9b10a6
Fayaze H. Pantho <60003361+Fayazexo@users.noreply.github.com>
2021-08-26 19:28:54 +0600

    Update candyfetch.jsx

--- src/utils/candyfetch.jsx
+++ src/utils/candyfetch.jsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://139.59.247.91/greydstudios',
-  });
+  baseURL: "http://139.59.247.91/greydstudios",
+})
 
-export default instance;
+export default instance
